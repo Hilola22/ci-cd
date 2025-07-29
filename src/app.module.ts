e6ca56dc52a95708bb188926df43ef3aba906cb5
Hilola22 <hilolaorazaliyeva2203@gmail.com>
2025-07-29 14:59:10 +0500

    added test workflow

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: ".env",
-    isGlobal: true
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
